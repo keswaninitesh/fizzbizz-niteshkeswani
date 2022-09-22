b6107ad24c528f3293d29ed4660ca8dd2ae22ced
keswaninitesh <113470200+keswaninitesh@users.noreply.github.com>
2022-09-22 13:51:41 -0300

    Initializing the program

---
+++ test1.py
@@ -0,0 +1,14 @@
+#Generating the first code
+#!/usr/bin/env python
+# Reference https://www.programsbuzz.com/interview-question/write-program-prints-numbers-1-100-multiples-three-print-fizz
+x_1 = 1
+x_101 = 101
+def fizzbuzz_niteshkeswani(x_1,x_101):
+	for num in range(x_1, x_101):
+		if num % 3 == 0 and num % 5 == 0:
+			print("FizzBuzz")
+			continue
+		elif num % 3 == 0:
+			print("Fizz" +str(num))
+		elif num % 5 == 0:
+			print("Buzz" +str(num))
